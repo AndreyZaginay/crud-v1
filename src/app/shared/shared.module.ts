e19c40021b77489e7e8bf9447952e7d398e8002e
AndreyZaginay <andrey201223@ukr.net>
2022-12-15 15:43:17 +0200

    add list component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from './components/components.module';
 
 
 
@@ -7,6 +8,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ComponentsModule]
 })
 export class SharedModule { }
